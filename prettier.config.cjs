622b61a9366b9390fc9b2e76f4b35b0e4ae3068c
Bruno Mello <bruno.barros.mello@gmail.com>
2023-01-03 15:14:44 -0300

    chore: eslint and prettier setup

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,7 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
 };
