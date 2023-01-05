6e60c2d185a922c9b5764de23a1bbb3021f1c8ba
Bruno Mello <bruno.barros.mello@gmail.com>
2023-01-05 12:08:22 -0300

    feat: finish basic layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
 import { type NextPage } from 'next';
-import { TimerCard } from '../components';
+import { Header, TimerCard } from '../components';
 
 const Home: NextPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start bg-gray-600">
-      <div className="container flex h-full flex-col items-center justify-start gap-10 p-4">
-        <h1 className="text-5xl font-bold tracking-tight text-white sm:text-[5rem]">
-          Multitimer
-        </h1>
-
+    <>
+      <Header />
+      <main className="flex h-full flex-col items-center justify-start bg-gray-600">
         <div
-          className="grid h-full w-full items-start justify-center gap-2 overflow-y-scroll"
+          className="grid h-full w-full items-start justify-center gap-2 p-4"
           style={{
             gridTemplateColumns: 'repeat(auto-fill, 350px)',
           }}
@@ -38,8 +35,8 @@ const Home: NextPage = () => {
           <TimerCard />
           <TimerCard />
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
