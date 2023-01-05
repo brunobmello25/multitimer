a38e1bd2b56476319a0ee76edc8191a946fb4c80
Bruno Mello <bruno.barros.mello@gmail.com>
2023-01-05 12:08:15 -0300

    feat: header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export function Header(): JSX.Element {
+  return (
+    <header className="sticky top-0 z-30 flex w-full items-center justify-center bg-gray-600 p-2">
+      <h1 className="text-5xl font-bold tracking-tight text-white sm:text-[5rem]">
+        Multitimer
+      </h1>
+    </header>
+  );
+}
